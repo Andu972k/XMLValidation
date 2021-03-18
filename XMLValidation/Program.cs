70fc0b8c39cb7e2f8fa53c62966bb17c8cfa7d34
Anders Genderskov Binder <heyanders@gmail.com>
2021-03-18 13:12:48 +0100

    Add project files.

---
+++ XMLValidation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLValidation
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            //doc.LoadXml(@"C:\Users\anders\KEA\SoftwareTesting\XML\XMLValidation\XMLValidation\Product.xml");
+            doc.Load(new StreamReader(@"C:\Users\anders\KEA\SoftwareTesting\XML\XMLValidation\XMLValidation\Product.xml"));
+            string text = "";
+            if (doc.DocumentElement != null)
+            {
+                foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+                {
+                    text += node.Name + ": " + node.InnerText;
+                }
+                Console.WriteLine(text);
+            }
+
+            
+            
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
